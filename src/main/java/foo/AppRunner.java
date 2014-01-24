deb5eef655bf5407d775ff835613bff908e3c53b
David Caramelo <david.caramelo83@gmail.com>
2014-01-24 23:16:11 +0100

    Extract read file method and create FileReader class utils

--- src/main/java/foo/AppRunner.java
+++ src/main/java/foo/AppRunner.java
@@ -1,10 +1,7 @@
 package foo;
 
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 import javax.naming.directory.InvalidAttributesException;
 
@@ -13,47 +10,27 @@ import org.apache.log4j.Logger;
 import foo.bean.Position;
 import foo.exception.FileFormatInvalidException;
 import foo.exception.InitialPositionMowerInvalidException;
+import foo.utils.FileReader;
 
 public class AppRunner {
 	
 	static final Logger logger = Logger.getLogger(AppRunner.class);
 	
-	public static void main(final String[] args) throws InvalidAttributesException, FileFormatInvalidException, InitialPositionMowerInvalidException {
+	public static void main(final String[] args) throws InvalidAttributesException, FileFormatInvalidException, InitialPositionMowerInvalidException, FileNotFoundException {
 	   
 		if(args == null || args.length == 0) {
 			logger.error("Filename is mandatory !");
 			throw new InvalidAttributesException();
 		}
-	   
-		List<String> fileLine = readFile(args);
-        
-		MowerControl mowerControl = new MowerControl(fileLine);
-        
-		List<Position> positions = mowerControl.process();
+	    
+		List<String> fileLine = FileReader.getInstance().read(args[0]);
+ 
+		List<Position> positions = new MowerControl(fileLine).process();
 		
 		logger.info("****************** Results **********************");
 		for (int i=0 ; i < positions.size() ; i++) {
 			logger.info("   " + i + " : Mower final position : " + positions.get(i));
 		}
 		logger.info("*************************************************");
-	}
-
-	private static List<String> readFile(final String[] args) {
-		List<String> fileLine = new ArrayList<String>();
-		   
-        File file = new File(args[0]);
- 
-        try {
-        	Scanner scanner = new Scanner(file);
- 
-        	while (scanner.hasNextLine()) {
-        		String line = scanner.nextLine();
-        		fileLine.add(line);
-        	}
-        	scanner.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-		return fileLine;
 	}
 }
